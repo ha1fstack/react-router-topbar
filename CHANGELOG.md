573d05126f5dae8bbfbd5707d8658177590d76c6
ha1fstack <61955474+ha1fstack@users.noreply.github.com>
2024-09-23 00:58:42 +0900

    chore: pack configs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,10 +4,4 @@ All notable, unreleased changes to this project will be documented in this file
 
 ## [Unreleased]
 
-- chore
-
-## [0.0.1-beta.1] - 2024-09-23
-
 - Initial release
-
-## [0.0.0]
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { Options } from 'tsup';
+import { Options } from "tsup";
 export const tsup: Options = {
-  target: 'es6',
+  target: "es6",
   clean: true,
   dts: true,
-  entry: ['src/index.tsx', 'src/pages.tsx', 'src/app.ts'],
+  entry: ["src/index.tsx"],
   keepNames: true,
   sourcemap: true,
   minify: true,
-  format: ['cjs'],
+  format: ["cjs", "esm"],
 };
